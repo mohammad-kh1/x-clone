b032a48bdaa1e31bcd160c3cfb0c131d1eec5fbc
Mohammad <D@gmail.com>
2024-11-11 02:44:20 -0800

    UI DESGIN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from "./components/common/RightPanel.jsx";
+import NotificationPage from "./pages/NotificationPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+const App = () => {
   return (
-    <>
-      <h1 className="text-red-500">s</h1>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
+    </div>
   );
-}
+};
 
 export default App;
